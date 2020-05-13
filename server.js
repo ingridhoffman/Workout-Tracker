d10cc7e13bf49a0c451160470a54605dd4abdfbf
trapezechic <ingridhoffman@me.com>
2020-05-13 12:30:46 -0700

    create html routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Node dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 // Set port and Express
 const PORT = process.env.PORT || 3000;
@@ -10,10 +11,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Set Mongoose connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+// Open connection to database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 	useNewUrlParser: true,
 	useFindAndModify: false,
+});
+
+// API routes
+app.use(require("./routes/api"));
+
+// HTML routes
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/exercise", function (req, res) {
+	res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+app.get("/stats", function (req, res) {
+	res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
 // Set to listen on port
