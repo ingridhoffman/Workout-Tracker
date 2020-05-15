7999992379bf7dd45f5952b897a5fc7a163d301d
trapezechic <ingridhoffman@me.com>
2020-05-14 18:00:16 -0700

    create api routes

---
+++ routes/api.js
@@ -0,0 +1,63 @@
+// Node dependencies
+const router = require("express").Router();
+
+// Local dependencies
+const db = require("../models");
+
+// GET - Find all workouts
+router.get("/api/workouts", (req, res) => {
+	db.Workout.find({})
+		.sort({ date: -1 })
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+// PUT - Update an existing workout with a new exercise
+router.put("/api/workouts/:id", (req, res) => {
+	db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true })
+		.then((dbWorkout) => {
+			console.log(dbWorkout);
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+// POST - Insert a new workout
+router.post("/api/workouts", (req, res) => {
+	console.log(req.body);
+	const date = new Date();
+	console.log("date: " + date);
+	const weekday = new Date().getDay();
+	console.log("weekday: " + weekday);
+	const workout = new db.Workout({ day: date, weekday: weekday });
+	// workout.setWeekday();
+	console.log("workout: " + workout);
+	db.Workout.create(workout)
+		.then((dbWorkout) => {
+			console.log(dbWorkout);
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+// GET - Find all workouts in last seven days
+router.get("/api/workouts/range", (req, res) => {
+	db.Workout.find({ day: { $gte: req.query.start, $lt: req.query.end } })
+		.sort({ day: 1 })
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+module.exports = router;
